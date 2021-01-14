0c753dd2b65efa4239618411955ee5932f9a2501
ivaanrl <ivanrl2010@gmail.com>
2021-01-14 17:35:21 -0300

    fix firebaseAdmin config

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -5,7 +5,8 @@ if (!firebaseAdmin.apps.length) {
     credential: firebaseAdmin.credential.cert({
       privateKey: process.env.private_key,
       clientEmail: process.env.client_email,
-      ['project_id' as 'projectId']: process.env.project_id,
+      //['project_id' as 'projectId']: process.env.project_id,
+      projectId: process.env.project_id,
     }),
     databaseURL: `https://feriadellibronecochea-default-rtdb.firebaseio.com/`,
     storageBucket: 'feriadellibronecochea.appspot.com',
