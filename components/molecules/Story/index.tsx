7c741c98c80cc8a64d984230337b802993a3d1f4
ivaanrl <ivanrl2010@gmail.com>
2021-01-15 14:48:23 -0300

    add hover animation to stories

--- components/molecules/Story/index.tsx
+++ components/molecules/Story/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, GridItem, Img, Text } from '@chakra-ui/react';
+import { Box, chakra, Flex, GridItem, Img, Text } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import { FC } from 'react';
 
 export interface StoryProps {
@@ -7,9 +8,24 @@ export interface StoryProps {
   imageUrl: string;
 }
 
+const MotionBox = chakra(motion.div);
+
 export const Story: FC<StoryProps> = ({ title, subtitle, imageUrl }) => {
   return (
-    <Flex direction="column" width="fit-content" margin="auto" mb="80px">
+    <MotionBox
+      display="flex"
+      flexDirection="column"
+      width="fit-content"
+      margin="auto"
+      mb="80px"
+      padding="20px"
+      borderRadius={20}
+      whileHover={{
+        borderRadius: '20px',
+        boxShadow: '10px 10px 38px -10px rgba(0,0,0,0.75)',
+        cursor: 'pointer',
+      }}
+    >
       <Box height="300px" width="100%" maxWidth="450px">
         <Img src={imageUrl} width="100%" height="100%" />
       </Box>
@@ -25,6 +41,6 @@ export const Story: FC<StoryProps> = ({ title, subtitle, imageUrl }) => {
       <Text color="text.light" fontSize="lg">
         {subtitle}
       </Text>
-    </Flex>
+    </MotionBox>
   );
 };
